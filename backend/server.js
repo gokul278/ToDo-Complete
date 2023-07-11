8851e7021f60203abc5fa4f6490146d8d9560d43
GOKUL M <82570950+gokul278@users.noreply.github.com>
2023-07-11 17:28:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({origin : "https://todo-complete-u6dv.onrender.com/"}))
+app.use(cors({
+  origin: 'https://todo-complete-u6dv.onrender.com',
+}));
 
 app.use('/',routes)
 
